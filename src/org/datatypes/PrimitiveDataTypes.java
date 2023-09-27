44327ab00677463d31bdaf0b0513a93e402a4c33
nazia1208 <sadhunaziaanjum@gmail.com>
2023-09-27 19:57:00 +0530

    This is a task given by Trainer

---
+++ src/org/datatypes/PrimitiveDataTypes.java
@@ -0,0 +1,29 @@
+package org.datatypes;
+
+public class PrimitiveDataTypes {
+	boolean a;
+	byte b;
+	char c;
+	short d;
+	int e;
+	long f;
+	float g;
+	double h;
+	char i;
+	
+	public static void main(String[] args)
+	{
+		PrimitiveDataTypes PDT=new PrimitiveDataTypes();
+		
+		System.out.println(PDT.a);
+		System.out.println(PDT.b);
+		System.out.println(PDT.c);
+		System.out.println(PDT.d);
+		System.out.println(PDT.e);
+		System.out.println(PDT.f);
+		System.out.println(PDT.g);
+		System.out.println(PDT.h);
+		System.out.println(PDT.i);
+		}
+	
+}
